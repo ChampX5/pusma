2a7bb1b9c82bd4a08cb857545bedd32fad40a4b9
ChampX5 <oshal.baranwal@gmail.com>
2023-03-29 09:02:03 +0530

    update: bootstrap, + images

--- index.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector('.navbar');
-
-const toggle = document.querySelector('.navbar-toggle');
-
-toggle.addEventListener('click', () => {
-    navbar.classList.toggle('navbar-open');
-    navbar.classList.toggle('navbar-closed');
-});
